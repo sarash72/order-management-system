26c2e84d3cfdd62c95ebe60887d477db3133242b
sara shirdel <sara.shirdel93@gmail.com>
2025-11-08 11:41:11 +0330

    Initial project structure for Order Management System

---
+++ src/main/java/org/example/ordermanagment/OrderManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.ordermanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManagmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ordermanagment/OrderManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ordermanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
