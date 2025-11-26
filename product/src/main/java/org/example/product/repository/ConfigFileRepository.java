101f5c4f6472e2afc2b47844c6c40f4603b15cc1
sara shirdel <sara.shirdel93@gmail.com>
2025-11-26 14:26:55 +0330

    process xml file

--- product/src/main/java/org/example/product/repository/ConfigFileRepository.java
+++ product/src/main/java/org/example/product/repository/ConfigFileRepository.java
@@ -2,9 +2,19 @@ package org.example.product.repository;
 
 import org.example.product.entity.ConfigFileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ConfigFileRepository extends JpaRepository<ConfigFileEntity,Long> {
 
     ConfigFileEntity findFirstByOrderByVersionDesc();
 
+
+    @Query(
+            value = "SELECT * FROM config_file_table " +
+                    "ORDER BY CAST(SUBSTRING(version, 2) AS UNSIGNED) DESC " +
+                    "LIMIT 1",
+            nativeQuery = true
+    )
+    ConfigFileEntity findLatestVersion();
 }
+
